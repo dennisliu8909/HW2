27856e675329b2e8017800664d217816c4f5960b
dennisliu8909 <76942544+dennisliu8909@users.noreply.github.com>
2021-03-31 15:02:32 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ AnalogIn ain(D11);
 int main() {
     int freq = 50;
     float T = 20000;
-    float value[1500];
+    float value[500];
     int count = 1;
-    int sample_num = 3;
+    int sample_num = 25;
 
     uLCD.text_width(2);
     uLCD.text_height(2);
@@ -25,21 +25,23 @@ int main() {
         if (button_up == 1) {
             freq += 10;   
             uLCD.locate(1, 2);
-            uLCD.printf("%4d\n", 1);
-            //  uLCD.printf("%4d", freq);
+            // uLCD.printf("%4d\n", count);
+             uLCD.printf("%4d", freq);
         }
         if (button_down == 1) {
             freq -= 10;
             uLCD.locate(1, 2);
-            uLCD.printf("%4d\n", 2);
-            // uLCD.printf("%4d", freq);
+            // uLCD.printf("%4d\n", i);
+            uLCD.printf("%4d", freq);
         }
         if (button_select == 1) {
             T = 1000000/freq;
             uLCD.locate(1, 2);
-            uLCD.printf("%4d\n", 3);
-            // uLCD.printf("%4d\n", freq);
-            sample_num = 100 * freq / 1500;
+            uLCD.printf("%4d\n", freq);
+            sample_num = 100 * freq / 500;
+            for (int k = 0; k < 500; k++) {
+                printf("%.3f\r\n", value[k]);
+            }
         }
         if (j >= 100) {
             j = 0;
@@ -51,17 +53,16 @@ int main() {
             aout = (100 - j) / 80.0f / 1.1;
         }
         j++;
-        // if (i >= 1500) {
-        //     i = 0;
-        //     count = 0;
-        //     for (int k = 0; k < 1500; k++) {
-        //         printf("%.3f\r\n", value[k]);
-        //     }
-        // } else if (count % sample_num == 0) {
-        //     value[i] = ain;
-        //     i++;
-        // }
-        // count++;
-        wait_us(T/100 - 40);
+
+        if (i >= 500) {
+            i = 0;
+            count = 0;
+        } else if (count % sample_num == 0) {
+            value[i] = ain;
+            // value = ain;
+            i++;
+        }
+        count++;
+        wait_us(T/100-23);
     }
 }
