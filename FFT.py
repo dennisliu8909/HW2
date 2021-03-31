78f18734ea206a7a2dc7a8fddbe95cae71364c8a
dennisliu8909 <76942544+dennisliu8909@users.noreply.github.com>
2021-03-31 15:04:45 +0800

    Update FFT.py

--- FFT.py
+++ FFT.py
@@ -3,7 +3,7 @@ import numpy as np
 import serial
 import time
 
-Fs = 10000.0;  # sampling rate
+Fs = 500.0;  # sampling rate
 Ts = 1.0/Fs; # sampling interval
 t = np.arange(0,1,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
 y = np.arange(0,1,Ts) # signal vector; create Fs samples
@@ -12,7 +12,7 @@ n = len(y) # length of the signal
 k = np.arange(n)
 T = n/Fs
 frq = k/T # a vector of frequencies; two sides frequency range
-frq = frq[range(int(n/2)] # one side frequency range
+frq = frq[range(int(n/2))] # one side frequency range
 
 serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev)
